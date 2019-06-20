66ebd65ad5f57c572081237d64f2c5eaffa2f8be
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-06-21 02:32:28 +0900

    [#8] Replace old json code of FCapsule with new codes

--- Include/Shape/FCapsule.hpp
+++ Include/Shape/FCapsule.hpp
@@ -17,6 +17,8 @@
 #include <Math/Type/Shape/DCapsule.h>
 #include <IHitable.hpp>
 #include <XCommon.hpp>
+#include <Helper/XJsonCallback.hpp>
+#include <Helper/EJsonExistance.hpp>
 
 namespace ray
 {
@@ -32,13 +34,24 @@ public:
     enum EType { _1, _2 };
     EXPR_INIT_ENUMTOTYPE(Type, EType);
 
-    struct PType1 final { DVec3 mOrigin; TReal mHeight; TReal mRadius; DVec3 mAngle; };
-    struct PType2 final { DVec3 mOrigin; TReal mLength; DVec3 mAngle; };
+    struct PType1 final 
+    { 
+      DVec3 mOrigin; TReal mHeight; TReal mRadius; DVec3 mAngle; 
+      PType1 Overwrite(const PType1& pctor, const json::FExistanceList& list) const;
+    };
+    struct PType2 final 
+    {
+      DVec3 mOrigin; TReal mLength; DVec3 mAngle; 
+      PType2 Overwrite(const PType2& pctor, const json::FExistanceList& list) const;
+    };
     EXPR_SET_ENUMTOTYPE_CONVERSION(Type, EType::_1, PType1);
     EXPR_SET_ENUMTOTYPE_CONVERSION(Type, EType::_2, PType2);
 
     EType mCtorType = EType::_1;
     std::variant<PType1, PType2> mCtor;
+
+    /// @brief Overwrite with given pctor instance and create new PCtor.
+    PCtor Overwrite(const PCtor& pctor, const json::FExistanceList& list) const;
   };
 
   /// @brief Construct DCapsule with FCapsule::PCtor.
@@ -53,6 +66,10 @@ public:
   /// @return When ray intersected to ray, returns TReal list.
   std::optional<std::vector<TReal>> GetRayIntersectedTValues(const DRay& ray) const override final;
 
+  /// @brief Get PCtor instance from instance, with given type value.
+  /// @param type Type value.
+  FCapsule::PCtor GetPCtor(FCapsule::PCtor::EType type) const noexcept;
+
 private:
   DQuat mRotQuat;
 };
@@ -62,5 +79,10 @@ private:
 void from_json(const nlohmann::json& json, FCapsule::PCtor& oCtor);
 void from_json(const nlohmann::json& json, FCapsule::PCtor::PType1& oCtor);
 void from_json(const nlohmann::json& json, FCapsule::PCtor::PType2& oCtor);
+
+/// @brief FCapsule specialized function.
+template <> json::FExistanceList JsonCheckExistances<FCapsule::PCtor>(const nlohmann::json& json);
+template <> json::FExistanceList JsonCheckExistances<FCapsule::PCtor::PType1>(const nlohmann::json& json);
+template <> json::FExistanceList JsonCheckExistances<FCapsule::PCtor::PType2>(const nlohmann::json& json);
 
 } /// ::ray namespace
