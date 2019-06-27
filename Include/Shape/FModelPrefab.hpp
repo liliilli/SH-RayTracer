58e74946fe39486820932c92a990fa065742c522
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-06-28 02:31:54 +0900

    [#4] Make model prefab type for separating to actual valid model type

---
+++ Include/Shape/FModelPrefab.hpp
@@ -0,0 +1,71 @@
+#pragma once
+///
+/// MIT License
+/// Copyright (c) 2018-2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <array>
+#include <filesystem>
+
+#include <nlohmann/json_fwd.hpp>
+#include <Expr/XEnumConversion.h>
+#include <IHitable.hpp>
+#include <XCommon.hpp>
+#include <Helper/XJsonCallback.hpp>
+#include <Helper/EJsonExistance.hpp>
+#include <Id/DModelId.hpp>
+#include <Shape/FModelMesh.hpp>
+#include <Shape/PModelCtor.hpp>
+
+namespace ray
+{
+
+/// @class FModelPrefab
+/// @brief Model type.
+class FModelPrefab final : public IHitable
+{
+public:
+  FModelPrefab(const PModelCtor& ctor, IMaterial* mat);
+  virtual ~FModelPrefab() = default;
+
+  /// @brief Get (x, y, z) position value.
+  const DVec3& GetOrigin() const noexcept;
+  /// @brief Get uniformized scale value.
+  const TReal& GetScale() const noexcept;
+  /// @brief Get angle quaternion.
+  const DQuat& GetQuaternion() const noexcept;
+
+  /// @brief Try to getting ray intersected t value list.
+  /// If this shape is not intersected with given ray, just return null value.
+  /// @param ray Ray of worls-space.
+  /// @return When ray intersected to ray, returns TReal list.
+  std::optional<TValueResults> GetRayIntersectedTValues(const DRay&) const override final 
+  {
+    return std::nullopt;
+  };
+
+  std::optional<PScatterResult> TryScatter(const DRay&, TReal) const override final 
+  {
+    return std::nullopt;
+  }
+
+  /// @brief Get PCtor instance from instance.
+  /// @param type Type value.
+  PModelCtor GetPCtor() const noexcept;
+
+private:
+  DVec3     mOrigin;
+  TReal     mScale;
+  DQuat     mRotQuat;
+  DModelId  mModelId;
+};
+
+} /// ::ray namespace
---
+++ Source/Shape/FModelPrefab.cc
@@ -0,0 +1,56 @@
+///
+/// MIT License
+/// Copyright (c) 2018-2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <Shape/FModelPrefab.hpp>
+#include <nlohmann/json.hpp>
+#include <XHelperJson.hpp>
+#include <Manager/MModel.hpp>
+
+namespace ray
+{
+
+FModelPrefab::FModelPrefab(const PModelCtor& ctor, IMaterial* mat)
+  : IHitable{EShapeType::Model, mat},
+    mOrigin { ctor.mOrigin },
+    mScale { ctor.mScale },
+    mRotQuat { ctor.mAngle },
+    mModelId { ctor.mModelResourceName }
+{ }
+
+PModelCtor FModelPrefab::GetPCtor() const noexcept
+{
+  PModelCtor result;
+  result.mOrigin  = this->GetOrigin();
+  result.mScale   = this->GetScale();
+  result.mAngle   = this->GetQuaternion().ToDegrees();
+  result.mModelResourceName = this->mModelId.ToString();
+
+  return result;
+}
+
+const DVec3& FModelPrefab::GetOrigin() const noexcept
+{
+  return this->mOrigin;
+}
+
+const TReal& FModelPrefab::GetScale() const noexcept
+{
+  return this->mScale;
+}
+
+const DQuat& FModelPrefab::GetQuaternion() const noexcept
+{
+  return this->mRotQuat;  
+}
+
+} /// ::ray namespace
